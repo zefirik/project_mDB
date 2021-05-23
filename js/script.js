81138452996286c0af491e498505d5349caea4d8
Artem <timredrick1@gmail.com>
2021-05-23 21:41:25 +0300

    practic lessons 2

---
+++ js/script.js
@@ -0,0 +1,102 @@
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+'use strict';
+
+// Возьмите свой код из предыдущей практики
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list'),
+      btn = document.querySelector('button'),
+      myInput = document.querySelector('.adding__input'),
+      chk = document.querySelector('.check');
+      
+
+      
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    let result = myInput.value;
+
+    if ( result== null || result == "" ) {
+        alert("Введите еазвание фильма");
+        return false;
+     }
+    
+     
+     if (result.length > 21) {
+        result =  result.substring(0, 20) + '...';
+        console.log(result);
+     }
+
+
+    movieDB.movies.push(result);
+
+    movieList.innerHTML = "";
+
+    movieDB.movies.sort();
+
+    movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+        `;
+    });
+    
+    
+    if (chk.checked != false) {
+        //alert('Добавляем любимый фильм;');
+       
+        console.log('Добавляем любимый фильм;')
+    };
+
+});
+
+ // And other try to git try4
